5c56aaa68e032e5ab881b4427486b506a9f5da2b
Joao Vale <jcarlos.vale@gmail.com>
2022-05-29 16:30:09 +0200

    Mono e Flux

--- src/main/java/com/study/service/FluxAndMonoGeneratorService.java
+++ src/main/java/com/study/service/FluxAndMonoGeneratorService.java
@@ -3,7 +3,10 @@ package com.study.service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class FluxAndMonoGeneratorService {
@@ -40,6 +43,153 @@ public class FluxAndMonoGeneratorService {
 
     public Flux<String> namesUppercaseHigherThanFour() {
         return Flux.fromIterable(List.of("alan", "bento", "carlao"));
+    }
+
+    public Flux<String> namesFlatMap() {
+        return Flux.fromIterable(List.of("alan", "bento"))
+                .flatMap(s -> splitString(s))
+                .log();
+    }
+
+    private Flux<String> splitString(String s) {
+        return Flux.fromArray(s.split(""));
+    }
+
+    public Flux<String> namesFlatMapAssync() {
+        return Flux.fromIterable(List.of("alan", "bento"))
+                .flatMap(s -> splitStringWithDelay(s))
+                .log();
+    }
+
+    private Flux<String> splitStringWithDelay(String s) {
+        var delay = new Random().nextInt(1000);
+        return Flux.fromArray(s.split("")).delayElements(Duration.ofMillis(delay));
+    }
+
+    public Flux<String> concatMap() {
+        return Flux.fromIterable(List.of("alan", "bento"))
+                .concatMap(s -> splitStringWithDelay(s))
+                .log();
+    }
+
+    public Mono<List<String>> flatMapWithMono() {
+        return Mono.just("java")
+                .map(s -> s.toUpperCase())
+                .flatMap(s -> splitStringMono(s));
+    }
+
+    private Mono<List<String>> splitStringMono(String s) {
+        return Mono.just(List.of(s.split("")));
+    }
+
+    public Flux<String> flatMapMany() {
+        return Mono.just("java")
+                .map(s -> s.toUpperCase())
+                .flatMapMany(s -> splitString(s));
+    }
+
+    public Flux<String> transform() {
+        Function<Flux<String>, Flux<String>> transformation =
+                flux -> flux.map(s -> s.toUpperCase()).filter(s -> s.startsWith("A"));
+
+        return Flux.fromIterable(List.of("alan", "bento"))
+                .transform(transformation)
+                .flatMap(s -> splitString(s))
+                .log();
+    }
+
+    public Flux<String> ifEmpty() {
+        return Flux.fromIterable(List.of("alan", "bento"))
+                .filter(s -> s.length() > 6)
+                .defaultIfEmpty("default value")
+                .log();
+    }
+
+    public Flux<String> switchIfEmpty() {
+
+        Flux<String> fluxDefault = Flux.just("Alan", "Bento");
+
+        return Flux.fromIterable(List.of("alan", "bento"))
+                .filter(s -> s.length() > 6)
+                .switchIfEmpty(fluxDefault)
+                .log();
+    }
+
+    public Flux<String> concat() {
+
+        var abcFlux = Flux.just("A", "B", "C");
+        var defFlux = Flux.just("D", "E", "F");
+
+        return Flux.concat(abcFlux, defFlux).log();
+    }
+
+    public Flux<String> concatWith() {
+
+        var aMono = Flux.just("A");
+        var bMono = Flux.just("B");
+
+        return aMono.concatWith(bMono).log();
+    }
+
+    public Flux<String> merge() {
+
+        var abcFlux = Flux.just("A", "B", "C").delayElements(Duration.ofMillis(100));
+        var defFlux = Flux.just("D", "E", "F").delayElements(Duration.ofMillis(125));
+
+        return Flux.merge(abcFlux, defFlux).log();
+    }
+
+    public Flux<String> mergeWith() {
+
+        var aMono = Mono.just("A");
+        var bMono = Mono.just("B");
+
+        return aMono.mergeWith(bMono).log();
+    }
+
+    public Flux<String> mergeSequential() {
+
+        var abcFlux = Flux.just("A", "B", "C").delayElements(Duration.ofMillis(100));
+        var defFlux = Flux.just("D", "E", "F").delayElements(Duration.ofMillis(125));
+
+        return Flux.mergeSequential(abcFlux, defFlux).log();
+    }
+
+    public Flux<String> zip() {
+
+        var abcFlux = Flux.just("A", "B", "C");
+        var defFlux = Flux.just("D", "E", "F");
+
+        return Flux.zip(abcFlux, defFlux, (a, b) -> a + b).log();
+    }
+
+    public Flux<String> otherZip() {
+
+        var abcFlux = Flux.just("A", "B", "C");
+        var defFlux = Flux.just("D", "E", "F");
+
+        var _123Flux = Flux.just("1", "2", "3");
+        var _456Flux = Flux.just("4", "5", "6");
+
+        return Flux.zip(abcFlux, defFlux, _123Flux, _456Flux)
+                .map(tuple -> tuple.getT1() + tuple.getT2() + tuple.getT3() + tuple.getT4())
+                .log();
+    }
+
+    public Flux<String> zipWith() {
+
+        var abcFlux = Flux.just("A", "B", "C");
+        var defFlux = Flux.just("D", "E", "F");
+
+        return abcFlux.zipWith(defFlux, (a, b) -> a + b).log();
+    }
+
+    public Mono<String> zipWithMono() {
+
+        var aMono = Mono.just("A");
+        var bMono = Mono.just("B");
+
+        return aMono.zipWith(bMono).map(tuple -> tuple.getT1() + tuple.getT2()).log();
     }
 
     public static void main(String[] args) {
--- src/test/java/com/study/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/study/service/FluxAndMonoGeneratorServiceTest.java
@@ -3,6 +3,8 @@ package com.study.service;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+
 class FluxAndMonoGeneratorServiceTest {
 
     String [] arrayOfNames = {
@@ -97,4 +99,225 @@ class FluxAndMonoGeneratorServiceTest {
                 .verifyComplete();
     }
 
+    @Test
+    void namesFlatMap() {
+        //given
+        //when
+        var flux = service.namesFlatMap();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("a","l","a","n","b","e","n","t","o")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlatMapWithDelay() {
+        //given
+        //when
+        var flux = service.namesFlatMapAssync();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNextCount(9)
+                .verifyComplete();
+    }
+
+    @Test
+    void concatMap() {
+        //given
+        //when
+        var flux = service.concatMap();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("a","l","a","n","b","e","n","t","o")
+                .verifyComplete();
+    }
+
+
+    @Test
+    void flatMapWithMono() {
+        //given
+        //when
+        var mono = service.flatMapWithMono();
+
+        //then
+        StepVerifier
+                .create(mono)
+                .expectNext(List.of("J","A","V","A"))
+                .verifyComplete();
+    }
+
+    @Test
+    void flatMapMany() {
+        //given
+        //when
+        var flux = service.flatMapMany();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("J","A","V","A")
+                .verifyComplete();
+    }
+
+    @Test
+    void transform() {
+        //given
+        //when
+        var flux = service.transform();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("A","L", "A", "N")
+                .verifyComplete();
+    }
+
+    @Test
+    void ifEmpty() {
+        //given
+        //when
+        var flux = service.ifEmpty();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("default value")
+                .verifyComplete();
+    }
+
+    @Test
+    void switchIfEmpty() {
+        //given
+        //when
+        var flux = service.switchIfEmpty();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("Alan", "Bento")
+                .verifyComplete();
+    }
+
+    @Test
+    void concat() {
+        //given
+        //when
+        var flux = service.concat();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+
+    @Test
+    void concatWith() {
+        //given
+        //when
+        var flux = service.concatWith();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("A", "B")
+                .verifyComplete();
+    }
+
+    @Test
+    void merge() {
+        //given
+        //when
+        var flux = service.merge();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("A", "D", "B", "E", "C", "F")
+                .verifyComplete();
+    }
+
+    @Test
+    void mergeWith() {
+        //given
+        //when
+        var flux = service.mergeWith();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("A", "B")
+                .verifyComplete();
+    }
+
+    @Test
+    void mergeSequential() {
+        //given
+        //when
+        var flux = service.mergeSequential();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+
+    @Test
+    void zip() {
+        //given
+        //when
+        var flux = service.zip();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("AD", "BE", "CF")
+                .verifyComplete();
+    }
+
+    @Test
+    void otherZip() {
+        //given
+        //when
+        var flux = service.otherZip();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("AD14", "BE25", "CF36")
+                .verifyComplete();
+    }
+
+    @Test
+    void zipWith() {
+        //given
+        //when
+        var flux = service.zipWith();
+
+        //then
+        StepVerifier
+                .create(flux)
+                .expectNext("AD", "BE", "CF")
+                .verifyComplete();
+    }
+
+    @Test
+    void zipWithMono() {
+        //given
+        //when
+        var mono = service.zipWithMono();
+
+        //then
+        StepVerifier
+                .create(mono)
+                .expectNext("AB")
+                .verifyComplete();
+    }
 }
