99e2cd956b043e4bcacc3673e23ec5fce872322f
Joao Vale <jcarlos.vale@gmail.com>
2022-06-01 00:03:16 +0200

    integration tests

--- src/main/java/com/study/controller/FluxAndMonoController.java
+++ src/main/java/com/study/controller/FluxAndMonoController.java
@@ -7,7 +7,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 
 @RestController
 public class FluxAndMonoController {
---
+++ src/main/java/com/study/model/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.study.model.repository;
+
+import com.study.model.UserDocument;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends ReactiveMongoRepository<UserDocument, String> {
+}
