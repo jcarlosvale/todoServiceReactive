737c68e344e7e8e72949e97b19ed150877473336
Joao Vale <jcarlos.vale@gmail.com>
2023-03-03 20:26:50 +0000

    ToDo Activity Integration

---
+++ src/main/java/com/study/dto/ActivityDto.java
@@ -0,0 +1,16 @@
+package com.study.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityDto {
+
+    @JsonProperty(value="activity")
+    private String description;
+
+}
