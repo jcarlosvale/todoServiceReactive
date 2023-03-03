d1e2fe0548974664a317eb56b0a25d64faca6b6b
Joao Vale <jcarlos.vale@gmail.com>
2023-03-03 14:10:07 +0000

    findByDate

--- src/main/java/com/study/repository/TodoInfoRepository.java
+++ src/main/java/com/study/repository/TodoInfoRepository.java
@@ -2,7 +2,11 @@ package com.study.repository;
 
 import com.study.domain.TodoInfo;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
 
 public interface TodoInfoRepository extends ReactiveMongoRepository<TodoInfo, String> {
 
+    Flux<TodoInfo> findByTodoDate(final LocalDate date);
 }
